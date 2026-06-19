408c3d9f17bda5528bb8123569c89f1c6e471ae0
Florian Piel <f.piel@campus.tu-berlin.de>
2026-06-19 14:55:11 +0200

    Added bcrypt password hashing

--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -2,18 +2,23 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '../user/user.entity';
 import { UsersService } from '../user/users.service';
 import { RegisterDto } from './register.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class RegisterService {
   constructor(private readonly usersService: UsersService) {}
   async register(registerData: RegisterDto): Promise<User> {
     return await this.usersService
       .findByUsername(registerData.username)
-      .then((user) => {
+      .then(async (user) => {
         if (user) {
           throw new BadRequestException('Username already exists');
         }
-        return this.usersService.createUser(registerData);
+        const saltOrRounds = 10;
+        registerData.password = await bcrypt.hash(
+          registerData.password,
+          saltOrRounds,
+        );
+        return await this.usersService.createUser(registerData);
       });
   }
 }
